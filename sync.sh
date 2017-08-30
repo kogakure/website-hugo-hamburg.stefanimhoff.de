cb6c91ed33776ee172b2e9dbe1268c8b3a66f2d2
Stefan Imhoff <stefan@imhoff.name>
2017-08-30 17:59:36 +0200

    Remove rsync task

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rsync --archive \
-      --verbose \
-      --recursive \
-      --checksum \
-      --delete \
-      --exclude '.DS_Store' \
-      ./build/production/ \
-      kogakure@stefanimhoff.de:webapps/hamburg/
