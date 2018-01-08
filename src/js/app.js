8d26b07ac904b91f0feb891e7df13bf830f49674
Stefan Imhoff <stefan@imhoff.name>
2018-01-08 16:37:12 +0100

    chore: Remove fastclick

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,10 @@
 import 'lazysizes';
 import fitvids from 'fitvids';
-import fastclick from 'fastclick';
 
 import {isTouchDevice} from './libs/is-touch-device';
 
 if ('querySelector' in document && 'addEventListener' in window) {
   document.addEventListener('DOMContentLoaded', function() {
-    // FastClick
-    fastclick.attach(document.body);
-
     // Fitvids
     fitvids('.main');
 
